583bb6a0c6f6d51c95fd061c9c22331896b855de
Sebi <sa48321@students.westport.k12.ct.us>
2015-06-08 19:25:13 -0400

    Initial

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'bundler'
 Bundler.require
 
+DB = Sequel.connect(ENV['DATABASE_URL'] || '//sqlite://db/main.db')
+require 'models.rb'
+
 get '/' do
   erb :index
+end
+
+get '/play' do
+  erb :main
 end
