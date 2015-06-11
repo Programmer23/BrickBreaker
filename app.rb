3d82da2b9b79908c09a88d71eaba0af697653096
Sebi <sa48321@students.westport.k12.ct.us>
2015-06-10 20:31:24 -0400

    Initial

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ use Rack::Session::Cookie, :key => 'rack.session',
     :expire_after => 2592000,
     :secret => SecureRandom.hex(64)
 
-include BCrypt
+#include BCrypt
 
 get '/' do
   erb :index
@@ -16,8 +16,8 @@ end
 
 post '/' do
   user = User.first(:name => params[:username])
-  if user && Password.new(user.password) == params[:password]
-    redirect '/main'
+  if user.name == params[:username] && user.password == params[:password]
+    redirect '/play'
   else
     @correct = false
     session["message"] = "Username or Password is incorrect"
@@ -36,7 +36,7 @@ post '/signup' do
   if (params[:new_password] == params[:new_password2])
     u = User.new
     u.name = params[:new_username]
-    u.password = Password.create(params[:new_password])
+    u.password = params[:new_password]
     u.save
   else
     session["message"] = "Passwords do not match"
